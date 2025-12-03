9d0c013195bf16b116e8f9cce82a1ce6664ddde5
Aayan Mateen <56849865+aayanmtn@users.noreply.github.com>
2025-12-04 01:01:08 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  adapter: netlify(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
